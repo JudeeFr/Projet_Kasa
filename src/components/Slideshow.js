24a881979e4d55b0db24fd68e6faf26a377ed774
Jennifer <jenniferbesson13@hotmail.com>
2023-07-13 12:09:09 +0200

    fix slideshow arrow display+ number of slides

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -14,6 +14,7 @@ export default function Slideshow({ slides }) {
 	const previousImg = () => {
 		setIndex((Index - 1 + slides.length) % slides.length);
 	};
+
 	return (
 		<div className="slideshow">
 			<img
@@ -21,16 +22,21 @@ export default function Slideshow({ slides }) {
 				src={slides[Index]}
 				alt={`Slide ${Index}`}
 			/>
+
+			<p className="number-slides">{`${Index}/` + slides.length}</p>
+
 			<img
 				src={arrowLeft}
 				alt="arrow left"
 				className="arrow-left"
+				style={{ display: slides.length == 1 ? 'none' : '' }}
 				onClick={previousImg}
 			/>
 			<img
 				src={arrowRight}
 				alt="arrow right"
 				className="arrow-right"
+				style={{ display: slides.length == 1 ? 'none' : '' }}
 				onClick={nextImg}
 			/>
 		</div>
